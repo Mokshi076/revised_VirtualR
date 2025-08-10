55ccc252fb033effeeef545f5abc96313800fbef
Mokshi <mokshi104rajak@gmail.com>
2025-08-11 02:34:13 +0530

    corrections

--- src/components/WorkFlow.jsx
+++ src/components/WorkFlow.jsx
@@ -1,5 +1,5 @@
 import { CircleCheck } from "lucide-react";
-import react from "../assets/code.png";
+import c from "../assets/code.png";
 import { checklistItems } from "../constants";
 const WorkFlow = () => {
   return (
@@ -15,6 +15,10 @@ const WorkFlow = () => {
                     <div key={index} className="flex mb-12">
                     <div className="text-green-400 mx-6 bg-neutral-900 h-10 w-10 p-2 justify-center items-center rounded-full">
                      <CircleCheck/>   </div>
+                     {/* <div>
+                      <h5 className="mt-1 mb-2 text-xl "> {item.title}</h5>
+                      <p className="text-md text-neutral-500"> {item.description}</p>
+                     </div> */}
                         </div>
                 ))}</div>
 

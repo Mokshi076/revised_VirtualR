b53da4fc88b3977032bfd896a4705f15a79e9f67
Mokshi <mokshi104rajak@gmail.com>
2025-08-11 18:56:41 +0530

    upto TestimonialSec

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import FeatureSec from "./components/FeatureSec";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
+import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
 import WorkFlow from "./components/WorkFlow";
 
 const App = () => {
@@ -10,9 +12,19 @@ const App = () => {
       <div className="max-w-7xl mx-auto pt-20 px-6">
       <HeroSection/>
       </div>
-      <FeatureSec/>
-      <WorkFlow/>
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <FeatureSec/>
+      </div>
+      <div className="max-w-6xl mx-auto pt-20 px-8">
+        <WorkFlow/>
+      </div>
       
+      <div className="max-w-6xl mx-auto pt-20 px-8">
+        <Pricing/>
+      </div>
+      <div className="max-w-6xl mx-auto pt-20 px-8">
+        <Testimonials/>
+      </div>
     </>
   );
 };
--- src/components/WorkFlow.jsx
+++ src/components/WorkFlow.jsx
@@ -1,10 +1,10 @@
 import { CircleCheck } from "lucide-react";
-import c from "../assets/code.png";
+import code from "../assets/code.png";
 import { checklistItems } from "../constants";
 const WorkFlow = () => {
   return (
         <div className="mt-20">
-            <h2 className="text-3xl sm:text-4xl lg:text-5xl text-center tracking-wide "> Accelerate Your <span className=" bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text">Coding Workflows</span></h2>
+            <h2 className="text-3xl sm:text-4xl lg:text-5xl text-center mt-6 tracking-wide "> Accelerate Your <span className=" bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text">Coding Workflows</span></h2>
             <div className="flex flex-wrap justify-center" >
                 <div className="p-2 w-full lg:w-1/2">
                 <img src={code} alt="code" />
@@ -15,10 +15,10 @@ const WorkFlow = () => {
                     <div key={index} className="flex mb-12">
                     <div className="text-green-400 mx-6 bg-neutral-900 h-10 w-10 p-2 justify-center items-center rounded-full">
                      <CircleCheck/>   </div>
-                     {/* <div>
+                     <div>
                       <h5 className="mt-1 mb-2 text-xl "> {item.title}</h5>
                       <p className="text-md text-neutral-500"> {item.description}</p>
-                     </div> */}
+                     </div>
                         </div>
                 ))}</div>
 
